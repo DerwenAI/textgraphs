46bb92cbd9b4e9f802fae4cb4ac9e3acc102e179
Paco Nathan <ceteri@gmail.com>
2024-01-02 14:02:54 -0800

    WIP construct a _graph of relations_ based on the InGram paper

--- gor.py
+++ gor.py
@@ -6,114 +6,143 @@ Experiment with deserializing a node-link graph,
 then transform it into a _graph of relations_
 """
 
-from collections import defaultdict
-from dataclasses import dataclass
+from collections import Counter, defaultdict
+from dataclasses import dataclass, field
+import enum
+import itertools
 import pathlib
 import json
+import sys
 import typing
 
 from icecream import ic  # pylint: disable=E0401
+import matplotlib.pyplot as plt  # pylint: disable=E0401
 import networkx as nx  # pylint: disable=E0401
 
 import textgraphs
 
 
-@dataclass(order=False, frozen=False)
-class DeserNode:  # pylint: disable=R0902
+class RelDir (enum.IntEnum):
     """
-A data class representing one deserialized Node.
+Enumeration for the directions of a relation.
+    """
+    HEAD = 0  # relation flows into node
+    TAIL = 1  # relation flows out of node
+
+    def __str__ (
+        self
+        ) -> str:
+        """
+Codec for representing as a string.
+        """
+        decoder: typing.List[ str ] = [
+            "head",
+            "tail",
+        ]
+
+        return decoder[self.value]
+
+
+@dataclass(order=False, frozen=False)
+class SheafSeed:
+    """
+A data class representing a node from the source graph plus its
+partial edge, based on a _Sheaf Theory_ decomposition of a graph.
     """
     node_id: int
-    base: str
-    label: str
+    rel_id: int
+    rel_dir: RelDir
+    edge: textgraphs.Edge
 
 
-NODE_LIST: typing.List[ DeserNode ] = []
-
-
-def gor_transform (
-    lemma_graph: nx.MultiDiGraph,
-    ) -> None:
+@dataclass(order=False, frozen=False)
+class TransArc:
     """
-Transform a `MultiDiGraph` into a _graph of relations_
+A data class representing one transformed rel-node-rel triple in
+a _graph of relations_.
     """
-    for node in lemma_graph.nodes:
-        n_attr: dict = lemma_graph.nodes[node]
-        kind: str = n_attr["kind"]
-
-        if kind in [ "iri" ]:
-            base: str = kind
-            label: str = n_attr["iri"]
-
-            NODE_LIST.append(DeserNode(
-                node,
-                base,
-                label,
-            ))
-
-        elif kind in [ "ent" ]:
-            base = n_attr["iri"]
-            label = n_attr["title"]
-
-            NODE_LIST.append(DeserNode(
-                node,
-                base,
-                label,
-            ))
-
-        elif kind in [ "chu", "dep", "lem" ]:
-            base = n_attr["pos"]
-            label = n_attr["title"]
-
-            NODE_LIST.append(DeserNode(
-                node,
-                base,
-                label,
-            ))
-
-        else:
-            ic(kind, n_attr)
+    pair_key: tuple
+    a_rel: int
+    b_rel: int
+    node_id: int
+    a_dir: RelDir
+    b_dir: RelDir
 
 
-def load_lemma () -> None:
+@dataclass(order=False, frozen=False)
+class Affinity:
     """
-Load a serialized lemma graph from JSON.
+A data class representing the affinity scores from one entity
+in the transformed _graph of relations_.
+
+NB: there are much more efficient ways to calculate these
+_affinity scores_ using sparse tensor algebra; this approach
+illustrates the process -- for research and debugging.
     """
-    lemma_path: pathlib.Path = pathlib.Path("lemma.json")
-
-    with open (lemma_path, "r", encoding = "utf-8") as fp:  # pylint: disable=C0103,W0621
-        dat: dict = json.load(fp)
-        lemma_graph: nx.MultiDiGraph = nx.node_link_graph(dat)
-        gor_transform(lemma_graph)
-
-    for edge in lemma_graph.edges:  # pylint: disable=W0621
-        ic(lemma_graph.edges[edge])
+    pairs: typing.Dict[ int, Counter ] = field(default_factory = lambda: defaultdict(Counter))
+    scores: typing.Dict[ int, float ] = field(default_factory = lambda: {})
+    tally: int = 0
 
 
-if __name__ == "__main__":
-    graph: textgraphs.SimpleGraph = textgraphs.SimpleGraph()
-    rel_list: typing.List[ str ] = []
-    head_links: typing.Dict[ int, set ] = defaultdict(set)
-    tail_links: typing.Dict[ int, set ] = defaultdict(set)
+class GraphOfRelations (textgraphs.SimpleGraph):
+    """
+Attempt to reproduce results published in
+"INGRAM: Inductive Knowledge Graph Embedding via Relation Graphs"
+<https://arxiv.org/abs/2305.19987>
+    """
 
-    with open("gor.json", "r", encoding = "utf-8") as fp:  # pylint: disable=W0621
-        triples: dict = json.load(fp)
+    def __init__ (
+        self
+        ) -> None:
+        """
+Constructor.
+        """
+        super().__init__()
 
-        for src_name, links in triples.items():
-            src_node: textgraphs.Node = graph.make_node(
-                [],
-                src_name,
-                None,
-                textgraphs.NodeEnum.ENT,
-                0,
-                0,
-                0,
-            )
+        self.rel_list: typing.List[ str ] = []
 
-            for rel_name, dst_name in links:
-                dst_node: textgraphs.Node = graph.make_node(
+        self.node_list: typing.List[ textgraphs.Node ] = []
+        self.edge_list: typing.List[ textgraphs.Edge ] = []
+
+        self.seed_links: typing.Dict[ int, list ] = defaultdict(list)
+
+        self.head_affin: typing.Dict[ int, Affinity ] = defaultdict(Affinity)
+        self.tail_affin: typing.Dict[ int, Affinity ] = defaultdict(Affinity)
+
+        # NB: should load these from the dataset?
+        self.pub_score: typing.Dict[ tuple, float ] = {
+            (0, 1): .22,
+            (0, 2): .50,
+            (1, 2): .33,
+            (1, 4): .11,
+            (2, 4): .11,
+            (3, 4): .81,
+            (3, 5): .11,
+            (4, 5): .36,
+        }
+
+
+    def load_ingram (
+        self,
+        json_file: pathlib.Path,
+        *,
+        debug: bool = False,
+        ) -> None:
+        """
+Load data for a source graph, as illustrated in _InGram_
+        """
+        with open(json_file, "r", encoding = "utf-8") as fp:  # pylint: disable=C0103,W0621
+            dat: dict = json.load(fp)
+
+            # JSON file provides an ordered list of relations
+            # to simplify tracing/debugging
+            self.rel_list = dat["rels"]
+
+            # build the src node of the triple
+            for src_name, links in dat["ents"].items():
+                src_node: textgraphs.Node = self.make_node(
                     [],
-                    dst_name,
+                    src_name,
                     None,
                     textgraphs.NodeEnum.ENT,
                     0,
@@ -121,25 +150,358 @@ if __name__ == "__main__":
                     0,
                 )
 
-                edge: textgraphs.Edge = graph.make_edge(  # type: ignore  # pylint: disable=W0621
-                    src_node,
-                    dst_node,
-                    textgraphs.RelEnum.SYN,
-                    rel_name,
-                    1.0,
+                for rel_name, dst_name in links:
+                    # error-check input
+                    if rel_name not in self.rel_list:
+                        print("Unknown relation:", rel_name)
+                        sys.exit(-1)
+
+                    # build the dst node of the triple
+                    dst_node: textgraphs.Node = self.make_node(
+                        [],
+                        dst_name,
+                        None,
+                        textgraphs.NodeEnum.ENT,
+                        0,
+                        0,
+                        0,
+                    )
+
+                    # create an edge between src/dst
+                    edge: textgraphs.Edge = self.make_edge(  # type: ignore  # pylint: disable=W0612,W0621
+                        src_node,
+                        dst_node,
+                        textgraphs.RelEnum.SYN,
+                        rel_name,
+                        1.0,
+                    )
+
+        if debug:
+            print(self.nodes)
+            print(self.edges)
+            print(self.rel_list)
+
+
+    def seeds (
+        self,
+        *,
+        debug: bool = False,
+        ) -> None:
+        """
+Prep data for the topological transform illustrated in _InGram_
+        """
+        self.node_list = list(self.nodes.values())
+        self.edge_list = list(self.edges.values())
+
+        if debug:
+            print("\n--- triples in source graph ---")
+
+        for edge in self.edges.values():
+            rel_id: int = self.rel_list.index(edge.rel)
+
+            if debug:
+                ic(edge.src_node, rel_id, edge.dst_node)
+                print("", self.node_list[edge.src_node].text, edge.rel, self.node_list[edge.dst_node].text)  # pylint: disable=C0301
+
+            # enumerate the partially decoupled links ("seeds")
+            # for the topological transform:
+            self.seed_links[edge.dst_node].append(SheafSeed(
+                edge.dst_node,
+                rel_id,
+                RelDir.HEAD,
+                edge,
+            ))
+
+            self.seed_links[edge.src_node].append(SheafSeed(
+                edge.src_node,
+                rel_id,
+                RelDir.TAIL,
+                edge,
+            ))
+
+
+    def trace_source_graph (
+        self
+        ) -> None:
+        """
+Output a "seed" representation of the source graph.
+        """
+        print("\n--- nodes in source graph ---")
+
+        for node in self.nodes.values():
+            # CONFIRMED: correct according to examples in the paper
+            print(f"n: {node.node_id:2}, {node.text}")
+
+            head_edges = [
+                ( seed.edge.src_node, seed.edge.rel, seed.edge.dst_node, )
+                for seed in self.seed_links[node.node_id]
+                if seed.rel_dir == RelDir.HEAD
+            ]
+
+            print("", "head:", head_edges)
+
+            tail_edges = [
+                ( seed.edge.src_node, seed.edge.rel, seed.edge.dst_node, )
+                for seed in self.seed_links[node.node_id]
+                if seed.rel_dir == RelDir.TAIL
+            ]
+
+            print("", "tail:", tail_edges)
+
+        print("\n--- edges in source graph ---")
+
+        for rel_id, rel in enumerate(self.rel_list):
+            print(f"e: {rel_id:2}, {rel}")
+
+
+    def _transformed_triples (
+        self,
+        *,
+        debug: bool = False,
+        ) -> typing.Iterator[ TransArc ]:
+        """
+Generate the transformed triples for a _graph of relations_.
+        """
+        for node_id, seeds in sorted(self.seed_links.items()):
+            if debug:
+                ic(node_id, len(seeds))
+
+            for seed_a, seed_b in itertools.combinations(seeds, 2):
+                pair_key: tuple = tuple(sorted([ seed_a.rel_id, seed_b.rel_id ]))
+
+                if debug:
+                    print(f" {pair_key} {seed_a.edge.rel}.{seed_a.rel_dir} {self.node_list[node_id].text} {seed_b.edge.rel}.{seed_b.rel_dir}")  # pylint: disable=C0301
+
+                trans_arc: TransArc = TransArc(
+                    pair_key,
+                    seed_a.rel_id,
+                    seed_b.rel_id,
+                    node_id,
+                    seed_a.rel_dir,
+                    seed_b.rel_dir,
                 )
 
-                if rel_name not in rel_list:
-                    rel_list.append(rel_name)
+                yield trans_arc
 
-                #ic(src_node.text, edge.rel, dst_node.text)
 
-                rel_id: int = rel_list.index(rel_name)
-                head_links[rel_id].add( edge.dst_node )
-                tail_links[rel_id].add( edge.src_node )
+    def construct_gor (
+        self,
+        *,
+        debug: bool = False,
+        ) -> None:
+        """
+Perform the topological transform described by _InGram_, constructing
+a _graph of relations_ (GOR) and calculating _affinity scores_ between
+entities in the GOR based on their definitions:
 
-    #print(graph.nodes)
-    #print(graph.edges)
-    #print(rel_list)
-    print(head_links)
-    print(tail_links)
+> we measure the affinity between two relations by considering how many
+entities are shared between them and how frequently they share the same
+entity
+        """
+        if debug:
+            print("\n--- transformed triples ---")
+
+        for trans_arc in self._transformed_triples(debug = debug):
+            if debug:
+                ic(trans_arc)
+                print()
+
+            if trans_arc.a_dir == RelDir.HEAD:
+                self.head_affin[trans_arc.a_rel].pairs[trans_arc.b_rel][trans_arc.node_id] += 1
+            else:
+                self.tail_affin[trans_arc.a_rel].pairs[trans_arc.b_rel][trans_arc.node_id] += 1
+
+            if trans_arc.b_dir == RelDir.HEAD:
+                self.head_affin[trans_arc.b_rel].pairs[trans_arc.a_rel][trans_arc.node_id] += 1
+            else:
+                self.tail_affin[trans_arc.b_rel].pairs[trans_arc.a_rel][trans_arc.node_id] += 1
+
+
+    @classmethod
+    def tally_frequencies (
+        cls,
+        counter: Counter,
+        ) -> int:
+        """
+Tally the frequency of shared entities.
+        """
+        sum_freq: int = counter.total()  # type: ignore
+
+        for occur in counter.values():  # pylint: disable=W0612
+            sum_freq += 1
+
+        return sum_freq
+
+
+    def _collect_tallies (
+        self,
+        *,
+        debug: bool = False,
+        ) -> None:
+        """
+Collect tallies, in preparation for calculating the affinity scores.
+        """
+        if debug:
+            print("\n--- collect shared entity tallies ---")
+
+        for rel_a, rel in enumerate(self.rel_list):
+            for rel_b, counter in sorted(self.head_affin[rel_a].pairs.items()):
+                tally: int = self.tally_frequencies(counter)
+                self.head_affin[rel_a].scores[rel_b] = float(tally)
+                self.head_affin[rel_a].tally += tally
+
+            for rel_b, counter in sorted(self.tail_affin[rel_a].pairs.items()):
+                tally = self.tally_frequencies(counter)
+                self.tail_affin[rel_a].scores[rel_b] = float(tally)
+                self.tail_affin[rel_a].tally += tally
+
+            if debug:
+                print(rel_a, rel)
+                print(" h:", self.head_affin[rel_a].tally, self.head_affin[rel_a].scores.items())
+                print(" t:", self.tail_affin[rel_a].tally, self.tail_affin[rel_a].scores.items())
+
+
+    def get_affinity_scores (
+        self,
+        *,
+        debug: bool = False,
+        ) -> typing.Dict[ tuple, float ]:
+        """
+Reproduce metrics based on the example published in _InGram_
+        """
+        self._collect_tallies(debug = debug)
+
+        if debug:
+            print("\n--- reproduce affinity scores ---")
+
+        scores: typing.Dict[ tuple, float ] = {}
+        n_rels: int = len(self.rel_list)
+
+        pairs: typing.Set[ tuple ] = {
+            tuple(sorted([ rel_a, rel_b ]))
+            for rel_a in range(n_rels)
+            for rel_b in range(n_rels)
+        }
+
+        for rel_a, rel_b in sorted(list(pairs)):
+            pair_affin: float = 0.0
+
+            if rel_b in self.head_affin and rel_a in self.tail_affin:
+                rel_a_sum = self.head_affin[rel_a].tally + self.tail_affin[rel_a].tally
+                a_contrib = self.tally_frequencies(self.head_affin[rel_b].pairs[rel_a])
+
+                rel_b_sum = self.head_affin[rel_b].tally + self.tail_affin[rel_b].tally
+                b_contrib = self.tally_frequencies(self.tail_affin[rel_a].pairs[rel_b])
+
+                pair_affin += (a_contrib / float(rel_a_sum)) + (b_contrib / float(rel_b_sum))
+
+            if rel_b in self.tail_affin and rel_a in self.head_affin:
+                rel_a_sum = self.head_affin[rel_a].tally + self.tail_affin[rel_a].tally
+                a_contrib = self.tally_frequencies(self.tail_affin[rel_b].pairs[rel_a])
+
+                rel_b_sum = self.head_affin[rel_b].tally + self.tail_affin[rel_b].tally
+                b_contrib = self.tally_frequencies(self.head_affin[rel_a].pairs[rel_b])
+
+                pair_affin += (a_contrib / float(rel_a_sum)) + (b_contrib / float(rel_b_sum))
+
+            if pair_affin > 0.0:
+                pair_key: tuple = tuple(sorted([ rel_a, rel_b ]))
+                scores[pair_key] = pair_affin / 2.0
+
+                if debug:
+                    pub: typing.Optional[ float ] = self.pub_score.get(pair_key)
+                    diff: float = 0.0
+
+                    if pub is not None:
+                        diff = (pub - scores[pair_key] ) / pub
+
+                    #ic(rel_a, rel_b)
+                    print(pair_key, self.rel_list[rel_a], self.rel_list[rel_b])
+                    print("", round(scores[pair_key], 2), self.pub_score.get(pair_key), diff)
+
+        return scores
+
+
+    def render_gor (
+        self,
+        scores: typing.Dict[ tuple, float ],
+        ) -> None:
+        """
+Visualize the _graph of relations_.
+        """
+        vis_graph: nx.Graph = nx.Graph()
+
+        vis_graph.add_nodes_from([
+            (
+                rel_id,
+                {
+                    "title": rel,
+                },
+            )
+            for rel_id, rel in enumerate(self.rel_list)
+        ])
+
+        vis_graph.add_edges_from([
+            (
+                rel_a,
+                rel_b,
+                {
+                    "weight": affinity,
+                },
+            )
+            for (rel_a, rel_b), affinity in scores.items()
+        ])
+
+        node_labels: typing.Dict[ int, str ] = dict(enumerate(self.rel_list))
+
+        edge_labels: typing.Dict[ int, str ] = {
+            edge_id: str(round(vis_graph.edges[edge_id]["weight"], 2))
+            for edge_id in vis_graph.edges
+        }
+
+        pos: dict = nx.spring_layout(
+            vis_graph,
+            k = 2.0,
+        )
+
+        nx.draw_networkx(
+            vis_graph,
+            pos,
+            labels = node_labels,
+            with_labels = True,
+            node_color = "#eee",
+            edge_color = "#bbb",
+            font_size = 9,
+        )
+
+        nx.draw_networkx_edge_labels(
+            vis_graph,
+            pos,
+            edge_labels = edge_labels,
+        )
+
+
+if __name__ == "__main__":
+    graph: GraphOfRelations = GraphOfRelations()
+
+    graph.load_ingram(
+        pathlib.Path("examples/ingram.json"),
+        debug = False,  # True
+    )
+
+    graph.seeds(
+        debug = True,  # False
+    )
+
+    graph.trace_source_graph()
+
+    graph.construct_gor(
+        debug = True,  # False
+    )
+
+    _scores: typing.Dict[ tuple, float ] = graph.get_affinity_scores(
+        debug = True,  # False
+    )
+
+    graph.render_gor(_scores)
+    plt.show()
